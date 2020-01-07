6b41b9f75d3f84665e16df47e49d5434088881f0
Evan Vogts <evanjvogts@gmail.com>
2020-01-07 10:12:54 -0600

    finished form and route - ready to merge and test

---
+++ capstone-3-client/src/routes/LoginRoute/login-route.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LoginForm from '../../components/LoginForm/login-form';
+
+export default class LoginRoute extends Component {
+  render() {
+    return (
+      <>
+        <LoginForm />
+      </>
+    )
+  }
+}
