9db164174a2d38bb2bd896f1c75f703dc72e58da
Evan Vogts <evanjvogts@gmail.com>
2020-01-07 12:21:45 -0600

    started navbar

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class NavBar extends Component {
+
+  renderLoginLink() {
+
+  }
+  renderLogoutLink() {
+
+  }
+  render() {
+    return (
+      <nav>
+        Home
+        Lean Drank
+        Breweries
+        Add
+      </nav>
+    )
+  }
+}
