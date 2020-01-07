c8cc08e9816e524d1c91bdca6f3d7efa55561324
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-07 11:50:32 -0700

    Update LandingPage.js

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,9 +2,8 @@ import React from "react";
 import {Link} from "react-router-dom";
 import './LandingPage.css'
 class LandingPage extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {
-
         this.state = {}
     }
 
