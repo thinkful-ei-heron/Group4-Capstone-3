8adcc63a25b30da95555a6db5eff67ffc3f838f9
Evan Vogts <evanjvogts@gmail.com>
2020-01-08 09:36:58 -0600

    built dashboard frame, and connected it to app.js/private route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import Register from "./Register/Register";
 import {Switch} from "react-router-dom";
 import PublicOnlyRoute from "./PublicOnlyRoute/PublicOnlyRoute";
+import PrivateOnlyRoute from './PrivateRoute/PrivateRoute';
 import LandingPage from "./LandingPage/LandingPage";
 import './App.css'
 import LoginForm from "./LoginForm/login-form";
 import TokenService from "./services/token-service";
+import Dashboard from './Dashboard/Dashboard';
 
 export default class App extends Component {
     state = { hasError: false };
@@ -24,6 +26,7 @@ export default class App extends Component {
                     <PublicOnlyRoute path={'/register'} component={Register}/>
                     <PublicOnlyRoute path={'/landing-page'} component={LandingPage}/>
                     <PublicOnlyRoute path={'/login'} component={LoginForm}/>
+                    <PrivateOnlyRoute path={'/home'} component={Dashboard}
                 </Switch>
 
             </div>
