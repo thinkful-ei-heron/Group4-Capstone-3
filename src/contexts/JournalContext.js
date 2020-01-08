efbb90b2178fea40912e1040f6c722f054596935
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-08 12:18:31 -0700

    Updated Changes

--- src/contexts/JournalContext.js
+++ src/contexts/JournalContext.js
@@ -33,52 +33,61 @@ export class JournalProvider extends Component {
       description: '',
       type: '',
       rating: 0,
-      abv: 0,
+      apv: 0,
       heaviness: 0,
       color: 0,
       image: 0
     };
-    this.setName = this.setName.bind(this);
-    this.setLoc = this.setName.bind(this);
-    this.setDesc = this.setName.bind(this);
-    this.setType = this.setName.bind(this);
-    this.setRating = this.setName.bind(this);
-    this.setAbv = this.setName.bind(this);
-    this.setHeaviness = this.setName.bind(this);
-    this.setColor = this.setName.bind(this);
-    this.setImage = this.setName.bind(this);
   }
+  setDate = date_created => {this.setState({ date_created })};
   setName = name => {this.setState({ name })};
   setLoc = location => {this.setState({ location })};
   setDesc = description => {this.setState({ description })};
   setType = type => {this.setState({ type })};
   setRating = rating => {this.setState({ rating })};
-  setAbv = abv => {this.setState({ abv })};
+  setApv = apv => {this.setState({ apv })};
   setHeaviness = heaviness => {this.setState({ heaviness })};
   setColor = color => {this.setState({ color })};
   setImage = image => {this.setState({ image })};
 
+  resetAll = () => {
+    this.setState({
+      name: '',
+      date_created: '',
+      location: '',
+      description: '',
+      type: '',
+      rating: 0,
+      apv: 0,
+      heaviness: 0,
+      color: 0,
+      image: 0
+    })
+  };
+  
   render() {
-    const value = {
+    let value = {
       name: this.state.name,
       date_created: this.state.date_created,
       location: this.state.location,
       description:  this.state.description,
       type:  this.state.type,
       rating: this.state.rating,
-      abv: this.state.abv,
+      apv: this.state.apv,
       heaviness: this.state.heaviness,
       color: this.state.color,
       image: this.state.image,
+      setDate: this.setDate,
       setName: this.setName,
       setLoc:  this.setLoc,
       setDesc: this.setDesc,
       setType: this.setType,
       setRating: this.setRating,
-      setAbv: this.setAbv,
+      setApv: this.setApv,
       setHeaviness: this.setHeaviness,
       setColor: this.setColor,
-      setImage: this.setImage
+      setImage: this.setImage,
+      resetAll: this.resetAll
     };
     return (
       <JournalContext.Provider value={value}>
