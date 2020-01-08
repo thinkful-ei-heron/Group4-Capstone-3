f9c5cdd8112074fcbacf8db12c886649fa9fbba3
Evan Vogts <evanjvogts@gmail.com>
2020-01-08 12:15:28 -0600

    used context logout function for logout button

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import UsersContext from '../../src/contexts/UserContext';
+import './NavBar.css';
 
 export default class NavBar extends Component {
 
-  renderLoginLink() {
+  static contextType = UsersContext;
 
+  handleLogoutClick = () => {
+    this.context.processLogout()
   }
-  renderLogoutLink() {
 
-  }
   render() {
     return (
       <nav>
-        <Link to={'/home'}>
-          Home
-        </Link>
-        <Link to={'/breweries'}>
-          Breweries
-        </Link>
-        <Link to={'/add'}>
-          Add+
-        </Link>
+        <button>
+          <Link to={'/home'}>
+            Home
+          </Link>
+        </button>
+        <button>
+          <Link to={'/breweries'}>
+            Breweries
+          </Link>
+        </button>
+        <button>
+          <Link to={'/add'}>
+            Add Beer
+          </Link>
+        </button>
+        <button onClick={this.handleLogoutClick}>
+          Logout
+        </button>
       </nav>
     )
   }
