e4ba3780daedfdcd95c243ee79d02d0e4a5716fd
Andrea Bender <andreab.1726@gmail.com>
2020-01-10 13:03:01 -0700

    changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
 	API_ENDPOINT: 'https://dearbeer-server.herokuapp.com', //'https://fathomless-caverns-68014.herokuapp.com',
-	TOKEN_KEY: 'kingpendata'
+	JWT_SECRET: 'kingpendata'
 };
