7ef28858dbd6e47a5137dc33e1d9e34fca1f6a42
Andrea Bender <andreab.1726@gmail.com>
2020-01-10 12:54:08 -0700

    updates

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',//'https://fathomless-caverns-68014.herokuapp.com',
-    TOKEN_KEY: 'kingpendata',
-}
+	API_ENDPOINT: 'https://dearbeer-server.herokuapp.com', //'https://fathomless-caverns-68014.herokuapp.com',
+	TOKEN_KEY: 'kingpendata'
+};
