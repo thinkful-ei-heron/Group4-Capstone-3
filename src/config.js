e9ceac4c4a6d72239393b67e8a9a83a05da645c2
Evan Vogts <evanjvogts@gmail.com>
2020-01-14 10:16:45 -0600

    change api_endpoint to localhost

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-	API_ENDPOINT: 'https://dearbeer-server.herokuapp.com/api', //'https://fathomless-caverns-68014.herokuapp.com',
+	API_ENDPOINT: 'http://localhost:8000/api', //'https://fathomless-caverns-68014.herokuapp.com',
 	JWT_SECRET: 'kingpendata'
 };
