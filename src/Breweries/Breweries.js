5481b23a22127e36491d8c7dfa94b7a72cabd75a
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-15 11:06:58 -0700

    Push

--- src/Breweries/Breweries.js
+++ src/Breweries/Breweries.js
@@ -4,6 +4,8 @@ import './Breweries.css';
 import Header from '../Header/Header';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker/Marker'
+import MapsApiService from '../services/maps-api-service'
+
 class Dashboard extends React.Component {
 
     static contextType = JournalContext;
@@ -12,14 +14,26 @@ class Dashboard extends React.Component {
         super(props);
         this.state = {
             center: {
-            lat: 59.95,
-            lng: 30.33},
-            zoom:11
+                lat: 59.95,
+                lng: 30.33
+            },
+            zoom: 11,
+            value: ''
         }
-
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
+    }
+
+    handleInput(e) {
+        this.setState({value: e.target.value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        MapsApiService.getArea(this.state.value).then(res=> console.log(res, 'hgasetheasjikdfh'))
     }
 
     render() {
@@ -27,9 +41,14 @@ class Dashboard extends React.Component {
             <>
                 <main className='breweries-page'>
                     <Header location={this.props.location} header={'Breweries'}/>
-                    <div style={{ height: '100vh', width: '100%' }}>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Zip Code: <input type='number' onChange={this.handleInput}
+                                                value={this.state.value}/></label>
+                        <button type='submit'>Go</button>
+                    </form>
+                    <div style={{height: '100vh', width: '100%'}}>
                         <GoogleMapReact
-                            bootstrapURLKeys={{ key:'AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g' }}
+                            bootstrapURLKeys={{key: 'AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g'}}
                             defaultCenter={this.state.center}
                             defaultZoom={this.state.zoom}
                         >
