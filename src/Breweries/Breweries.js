c0a4f748c710f2f860676d5cfe3a3367c3aff3a8
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-16 14:01:47 -0700

    Major Updates

--- src/Breweries/Breweries.js
+++ src/Breweries/Breweries.js
@@ -5,8 +5,10 @@ import Header from '../Header/Header';
 import GoogleMapReact from 'google-map-react';
 import BarMarker from './Marker/BarMarker'
 import BreweryMarker from './Marker/BreweryMarker'
+import InfoMarker from './Marker/InfoMarker'
 import MapsApiService from '../services/maps-api-service'
 import ListComponent from './ListComponent/ListComponent'
+
 class Dashboard extends React.Component {
 
     static contextType = JournalContext;
@@ -27,44 +29,33 @@ class Dashboard extends React.Component {
             nearByBreweries: [],
             list: [],
             value: '',
-            dropDown: 8047
+            dropDown: 8047,
+            selectedMarker: {lat: null, lng: null, name: '', vicinity: ''}
         };
         this.handleInput = this.handleInput.bind(this);
-        // this.handleDropdown = this.handleDropdown.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleInput(e) {
-
         this.setState({value: e.target.value})
     }
-
-    // handleDropdown(e) {
-    //     // console.log(e.target.value);
-    //     this.setState({dropDown: e.target.value})
-    // }
 
     handleSubmit(e) {
         e.preventDefault();
         MapsApiService.getArea({zipcode: this.state.value}).then(res => {
             let {lat, lng} = res.results;
-            console.log(lat, lng)
+            let zipcode = Math.sqrt((lat * lat) + (lng * lng));
             let list = [...res.breweries, ...res.bars];
-            list.sort((a,b)=> {
-                let zipcode = Math.sqrt((lat*lat)+(lng*lng));
-                let x = Math.abs(Math.sqrt((a.geometry.location.lat *a.geometry.location.lat )+(a.geometry.location.lng *a.geometry.location.lng ))-zipcode);
-                let y = Math.abs(Math.sqrt((b.geometry.location.lat *b.geometry.location.lat )+(b.geometry.location.lng *b.geometry.location.lng ))-zipcode);
-               return x-y;
+            list.sort((a, b) => {
+                let x = Math.abs(Math.sqrt((a.geometry.location.lat * a.geometry.location.lat) + (a.geometry.location.lng * a.geometry.location.lng)) - zipcode);
+                let y = Math.abs(Math.sqrt((b.geometry.location.lat * b.geometry.location.lat) + (b.geometry.location.lng * b.geometry.location.lng)) - zipcode);
+                return x - y;
             });
-            this.setState({center: {lat, lng}, nearByBars: res.bars, nearByBreweries: res.breweries, list});
+            this.setState({center: {lat, lng}, nearByBars: res.bars, nearByBreweries: res.breweries, list: list});
         })
     }
 
     render() {
-        // let distances = [{meters: 8047, miles: 5}, {meters: 16093.4, miles: 10}, {
-        //     meters: 24140.2,
-        //     miles: 15
-        // }, {meters: 32187, miles: 20}, {meters: 40233.6, miles: 25}];
         return (
             <>
                 <main className='breweries-page'>
@@ -72,9 +63,6 @@ class Dashboard extends React.Component {
                     <form onSubmit={this.handleSubmit}>
                         <label>Zip Code: <input type='number' onChange={this.handleInput}
                                                 value={this.state.value}/></label>
-                        {/*<select onChange={this.handleDropdown}>*/}
-                        {/*    {distances.map((dist, i) => <option key={i} value={dist.meters}>{dist.miles}</option>)}*/}
-                        {/*</select>*/}
                         <button type='submit'>Go</button>
                     </form>
                     <div
@@ -85,6 +73,12 @@ class Dashboard extends React.Component {
                                     center: {
                                         lat: place.geometry.location.lat,
                                         lng: place.geometry.location.lng
+                                    },
+                                    selectedMarker: {
+                                        lat: place.geometry.location.lat,
+                                        lng: place.geometry.location.lng,
+                                        name: place.name,
+                                        vicinity: place.vicinity
                                     }
                                 })}><ListComponent place={place}/></div>
                             )}
@@ -100,15 +94,19 @@ class Dashboard extends React.Component {
                                 lat={place.geometry.location.lat}
                                 lng={place.geometry.location.lng}
                                 text={place.name}
-                                isBeer={false}
                             />)}
                             {this.state.nearByBreweries.map((place, i) => <BreweryMarker
                                 key={i}
                                 lat={place.geometry.location.lat}
                                 lng={place.geometry.location.lng}
                                 text={place.name}
-                                isBeer={false}
                             />)}
+                            {this.state.selectedMarker.name.length !== 0 ? <InfoMarker
+                                lat={this.state.selectedMarker.lat}
+                                lng={this.state.selectedMarker.lng}
+                                name={this.state.selectedMarker.name}
+                                vicinity={this.state.selectedMarker.vicinity}
+                            /> : ''}
                         </GoogleMapReact>
                     </div>
                 </main>
---
+++ src/Breweries/Marker/InfoMarker.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import JournalContext from '../../contexts/JournalContext'
+import './Marker.css'
+import {Link} from "react-router-dom";
+
+export default class InfoMarker extends Component {
+    static contextType = JournalContext;
+
+    render() {
+        const K_WIDTH = 100;
+        const K_HEIGHT = 50;
+        const greatPlaceStyle = {
+            width: K_WIDTH,
+            left: -K_WIDTH / 2,
+            top: -K_HEIGHT * 2.25,
+        };
+        return (
+            <div className={'info-marker'} style={greatPlaceStyle}>
+                <p>{this.props.name}</p>
+                {/*// <img src={StarMarker} style={greatPlaceStyle} alt={'Marker'}/>*/}
+                <Link to={'/add'}>
+                <button onClick={() => this.context.setLoc(this.props.vicinity)}>Add Beer
+                </button>
+                </Link>
+            </div>
+        );
+    }
+}
