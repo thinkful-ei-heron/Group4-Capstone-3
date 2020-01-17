3960a0cd3eef3e43fbfef05a2b7320de7555bba2
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-17 13:40:28 -0700

    Created Map Context

--- src/Breweries/Breweries.js
+++ src/Breweries/Breweries.js
@@ -26,25 +26,19 @@ class Dashboard extends React.Component {
                 lng: -105.3716684
             },
             zoom: 11,
-            nearByBars: [],
-            nearByBreweries: [],
-            list: [],
-            value: '',
-            selectedMarker: {lat: null, lng: null, name: '', vicinity: '', id: ''}
         };
-        this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.updateSelected = this.updateSelected.bind(this);
-        this.resetSelected = this.resetSelected.bind(this);
     }
 
-    handleInput(e) {
-        this.setState({value: e.target.value})
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.map !== this.props.map) {
+            this.forceUpdate();
+        }
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        MapsApiService.getArea({zipcode: this.state.value}).then(res => {
+        MapsApiService.getArea({zipcode: this.props.map.value}).then(res => {
             let {lat, lng} = res.results;
             let zipcode = Math.sqrt((lat * lat) + (lng * lng));
             let list = [...res.breweries, ...res.bars];
@@ -53,81 +47,77 @@ class Dashboard extends React.Component {
                 let y = Math.abs(Math.sqrt((b.geometry.location.lat * b.geometry.location.lat) + (b.geometry.location.lng * b.geometry.location.lng)) - zipcode);
                 return x - y;
             });
-            this.setState({center: {lat, lng}, nearByBars: res.bars, nearByBreweries: res.breweries, list: list});
+            this.props.map.setList(list);
+            this.props.map.setBars(res.bars);
+            this.props.map.setBreweries(res.breweries);
+            this.props.map.setCenter( {lat, lng});
         })
-        
     }
-
-    updateSelected(id) {
-        let NewPlace = this.state.list.find((place) => place.id === id);
-        this.setState({selectedMarker: {
-                lat: NewPlace.geometry.location.lat,
-                lng: NewPlace.geometry.location.lng,
-                name: NewPlace.name,
-                vicinity: NewPlace.vicinity,
-                id: NewPlace.id
-            }});
+    setActive(data){
+        this.props.map.setCenter({lat: data.center.lat, lng: data.center.lng});
+        this.props.map.setSelected(data.selectedMarker);
 
     }
-    resetSelected(){
-        this.setState({selectedMarker: {lat: null, lng: null, name: '', vicinity: '', id: ''}});
-    }
+
     render() {
         return (
             <>
                 <main className='breweries-page'>
                     <Header location={this.props.location} header={'Breweries'}/>
                     <form onSubmit={this.handleSubmit}>
-                        <label>Zip Code: <input type='number' onChange={this.handleInput}
-                                                value={this.state.value}/></label>
+                        <label>Zip Code: <input type='number'
+                                                onChange={(e) => this.props.map.setZipcode(e.target.value)}
+                                                value={this.props.map.value}/></label>
                         <button type='submit'>Go</button>
                     </form>
                     <div className={'breweries-map'}>
                         <div className={'breweries-list'}>
-                            {this.state.list.map((place, i) =>
-                                <div key={i} className={(place.id===this.state.selectedMarker.id) ? 'breweries-list-active' : ''} onClick={() => this.setState({
-                                    center: {
-                                        lat: place.geometry.location.lat,
-                                        lng: place.geometry.location.lng
-                                    },
-                                    selectedMarker: {
-                                        lat: place.geometry.location.lat,
-                                        lng: place.geometry.location.lng,
-                                        name: place.name,
-                                        vicinity: place.vicinity,
-                                        id: place.id
-                                    }
-                                })}><ListComponent place={place} /></div>
+                            {this.props.map.list.map((place, i) =>
+                                <div key={i}
+                                     className={(place.id === this.props.map.selectedMarker.id) ? 'breweries-list-active' : ''}
+                                     onClick={() => this.setActive({
+                                         center: {
+                                             lat: place.geometry.location.lat,
+                                             lng: place.geometry.location.lng
+                                         },
+                                         selectedMarker: {
+                                             lat: place.geometry.location.lat,
+                                             lng: place.geometry.location.lng,
+                                             name: place.name,
+                                             vicinity: place.vicinity,
+                                             id: place.id
+                                         }
+                                     })}><ListComponent place={place}/></div>
                             )}
                         </div>
                         <GoogleMapReact
                             bootstrapURLKeys={{key: 'AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g'}}
                             defaultCenter={this.state.defaultCenter}
                             defaultZoom={this.state.zoom}
-                            center={this.state.center}
+                            center={this.props.map.center}
                         >
-                            {this.state.nearByBars.map((place, i) => <BarMarker
+                            {this.props.map.nearByBars.map((place, i) => <BarMarker
                                 key={i}
                                 lat={place.geometry.location.lat}
                                 lng={place.geometry.location.lng}
                                 text={place.name}
                                 id={place.id}
-                                updateSelected={this.updateSelected}
+                                updateSelected={this.props.map.updateSelected}
                             />)}
-                            {this.state.nearByBreweries.map((place, i) => <BreweryMarker
+                            {this.props.map.nearByBreweries.map((place, i) => <BreweryMarker
                                 key={i}
                                 lat={place.geometry.location.lat}
                                 lng={place.geometry.location.lng}
                                 text={place.name}
                                 id={place.id}
-                                updateSelected={this.updateSelected}
+                                updateSelected={this.props.map.updateSelected}
                             />)}
-                            {this.state.selectedMarker.name.length !== 0 ? <InfoMarker
-                                lat={this.state.selectedMarker.lat}
-                                lng={this.state.selectedMarker.lng}
-                                name={this.state.selectedMarker.name}
-                                vicinity={this.state.selectedMarker.vicinity}
-                                resetSelected={this.resetSelected}
+                            {this.props.map.selectedMarker.name.length !== 0 ? <InfoMarker
+                                lat={this.props.map.selectedMarker.lat}
+                                lng={this.props.map.selectedMarker.lng}
+                                name={this.props.map.selectedMarker.name}
+                                vicinity={this.props.map.selectedMarker.vicinity}
+                                resetSelected={this.props.map.resetSelected}
                             /> : ''}
                         </GoogleMapReact>
                         <KeyComponent/>
--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './contexts/UserContext';
-import { JournalProvider } from './contexts/JournalContext';
-import { ParallaxProvider } from 'react-scroll-parallax';
+import {BrowserRouter} from 'react-router-dom';
+import {UserProvider} from './contexts/UserContext';
+import {JournalProvider} from './contexts/JournalContext';
+import {MapProvider} from './contexts/MapContext';
+import {ParallaxProvider} from 'react-scroll-parallax';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<UserProvider>
-			<JournalProvider>
-				<ParallaxProvider>
-					<App />
-				</ParallaxProvider>
-			</JournalProvider>
-		</UserProvider>
-	</BrowserRouter>,
-	document.getElementById('root')
+    <BrowserRouter>
+        <UserProvider>
+            <JournalProvider>
+                <MapProvider>
+                    <ParallaxProvider>
+                        <App/>
+                    </ParallaxProvider>
+                </MapProvider>
+            </JournalProvider>
+        </UserProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
