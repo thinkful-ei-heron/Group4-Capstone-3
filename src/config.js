e70e39452478a3f63a925b6589213be2cb806044
Andrea Bender <andreab.1726@gmail.com>
2020-01-20 09:01:35 -0700

    deploy changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-	API_ENDPOINT: 'https://dearbeer-server.herokuapp.com',
+	API_ENDPOINT: 'https://dearbeer-server.herokuapp.com/api',
 	JWT_SECRET: 'kingpendata',
 	API_KEY: 'AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g'
 };
