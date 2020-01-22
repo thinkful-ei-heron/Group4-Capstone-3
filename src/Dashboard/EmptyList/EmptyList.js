8dfae0ef7431b6338447106625808e34375c4177
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-22 12:33:40 -0700

    Warning Fixes

--- src/Dashboard/EmptyList/EmptyList.js
+++ src/Dashboard/EmptyList/EmptyList.js
@@ -2,15 +2,10 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import './EmptyList.css';
 class EmptyList extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return( <div className={'empty-list'}>
             <h2>Drink Some Beer You Filthy Animal</h2>
-            <Link to={'/add/tutorial'}>Add Beer</Link>
-
+            <Link to={'/add/tutorial'}>Add Your First Beer</Link>
         </div>)
     }
 }
