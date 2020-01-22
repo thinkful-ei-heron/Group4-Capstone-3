8f03d08de3afe97ded699adc9292c8ef0c177cd3
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-22 14:29:06 -0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import ReactDOM from 'react-dom'
+import App from './App'
+import {BrowserRouter} from 'react-router-dom'
+import {UserProvider} from './contexts/UserContext';
+import {JournalProvider} from './contexts/JournalContext';
+import {MapProvider} from './contexts/MapContext';
+import {ParallaxProvider} from 'react-scroll-parallax';
+it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+        <BrowserRouter>
+            <UserProvider>
+                <JournalProvider>
+                    <MapProvider>
+                        <ParallaxProvider>
+                            <App/>
+                        </ParallaxProvider>
+                    </MapProvider>
+                </JournalProvider>
+            </UserProvider>
+        </BrowserRouter>,
+        div
+    )
+    ReactDOM.unmountComponentAtNode(div)
 });
