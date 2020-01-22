993fa20a02295e636eb7091b959bb07e838ef389
Maria Danielson <mal3905@gmail.com>
2020-01-22 15:38:27 -0500

    testing in prog

--- src/AddForm/AddForm.test.js
+++ src/AddForm/AddForm.test.js
@@ -4,8 +4,19 @@ import toJson from 'enzyme-to-json'
 import AddForm from './AddForm'
 
 describe(`AddForm component`, () => {
+    const props = {
+        className: 'test-class-name',
+        children: <p>test children</p>,
+        'data-other': 'test-other-prop'
+      }
+
   it('renders the complete form', () => {
     const wrapper = shallow(<AddForm />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('renders the EmmaForm given props', () => {
+    const wrapper = shallow(<AddForm {...props} />)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
