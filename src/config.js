3937c0c03b4dafb4065705602255c3fe81329e04
Evan Vogts <evanjvogts@gmail.com>
2020-01-23 14:30:42 -0600

    fixed dash list css

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-	API_ENDPOINT: process.env.API_ENDPOINT,
+	API_ENDPOINT: 'http://localhost:8000/api',
+	// API_ENDPOINT: process.env.API_ENDPOINT,
 	JWT_SECRET: process.env.JWT_SECRET,
 	API_KEY: process.env.API_KEY
 };
