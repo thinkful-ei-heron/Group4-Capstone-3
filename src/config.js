5c171feb0bb3be604c81f17564f5d01143646683
Andrea Bender <andreab.1726@gmail.com>
2020-01-23 11:53:30 -0700

    updates

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-	API_ENDPOINT: process.env.API_ENDPOINT,
-	JWT_SECRET: process.env.JWT_SECRET,
-	API_KEY: process.env.API_KEY
+	API_ENDPOINT: process.env.api_endpoint,
+	JWT_SECRET: process.env.jwt_secret,
+	API_KEY: process.env.api_key
 };
